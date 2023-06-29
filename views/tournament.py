dc781389e02a75df4bdeb156d97a781c592cda7e
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-06-29 10:19:06 +0200

    Tournament views added

--- views/tournament.py
+++ views/tournament.py
@@ -1,34 +1,38 @@
 class TournamentView:
-    def __init__(self, controller):
-        self.controller = controller
+    """Tournament view class"""
 
-    def create_tournament_prompt(self):
-        name = input("Enter the tournament name: ")
-        location = input("Enter the tournament location: ")
-        start_date = input("Enter the tournament start date (YYYY-MM-DD): ")
-        end_date = input("Enter the tournament end date (YYYY-MM-DD): ")
-        self.controller.create_tournament(name, location, start_date, end_date)
+    def display_tournament_menu(self):
+        """Display tournament main menu and return user's choice"""
 
-    def add_player_prompt(self):
-        first_name = input("Enter the player's first name: ")
-        last_name = input("Enter the player's last name: ")
-        date_of_birth = input("Enter the player's date of birth (YYYY-MM-DD): ")
-        self.controller.add_player(first_name, last_name, date_of_birth)
+        while True:
+            print("\n----- Tournament Menu -----\n")
+            print("1. Create Tournament")
+            print("2. Display Tournaments")
+            print("3. Exit")
 
-    def generate_matches(self):
-        self.controller.generate_matches()
+            choice = input("Enter your choice: ")
 
-    def enter_match_results_prompt(self):
-        results = []
-        for match in self.controller.tournament.matches:
-            print(f"Match: {match.player1.first_name} vs {match.player2.first_name}")
-            score_player1 = input("Enter the score for player 1: ")
-            score_player2 = input("Enter the score for player 2: ")
-            results.append((score_player1, score_player2))
-        self.controller.enter_match_results(results)
+            if choice in ["1", "2", "3"]:
+                return choice
+            else:
+                print("Invalid choice. Please try again.")
 
-    def display_players(self):
-        self.controller.display_players()
+    def get_tournament_info(self):
+        """Get tournament information from user"""
 
-    def display_tournaments(self):
-        self.controller.display_tournaments()
+        print("\nEnter Tournament Information:")
+        name = input("Name: ")
+        location = input("Location: ")
+        date = input("Date: ")
+
+        return {"name": name, "location": location, "date": date}
+
+    def display_tournaments(self, tournaments):
+        """Display a list of tournaments"""
+
+        print("\nTournaments:")
+        for tournament in tournaments:
+            print(f"Name: {tournament.name}")
+            print(f"Location: {tournament.location}")
+            print(f"Date: {tournament.date}")
+            print("-------------------------------")
